6b4265701baac1abc74f8bac4a3fbfd65a9bccdc
JonathanEmanoel <jonathan.emanoel23@gmail.com>
2024-11-10 22:58:57 -0300

    Novas pastas

---
+++ lib/Models/animals.dart
@@ -0,0 +1,18 @@
+import 'dart:ffi';
+
+class   Animals {
+  String name;
+  Int chip;
+  
+
+  Animals({
+    required this.name,
+    required this.chip,
+
+  });
+
+  String get _name => name;
+
+
+
+}
